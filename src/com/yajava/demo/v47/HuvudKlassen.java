df2db7070eda9037fe8c358c148e074f27e03201
Teddyborg <anders.tedenborg@gmail.com>
2021-11-24 09:32:15 +0000

    huvudklass ändrad

--- src/com/yajava/demo/v47/HuvudKlassen.java
+++ src/com/yajava/demo/v47/HuvudKlassen.java
@@ -3,7 +3,7 @@ package com.yajava.demo.v47;
 public class HuvudKlassen {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to demo");
 
 	}
 
